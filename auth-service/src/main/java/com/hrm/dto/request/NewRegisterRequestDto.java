cbdae7a7bae6bacb0aeabeed9cb0484c3954ebbc
ErincK <erinckocaoglu@gmail.com>
2023-04-14 02:10:27 +0300

    AuthService done

---
+++ auth-service/src/main/java/com/hrm/dto/request/NewRegisterRequestDto.java
@@ -0,0 +1,28 @@
+package com.hrm.dto.request;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class NewRegisterRequestDto {
+
+    @Email
+    private String email;
+    @NotBlank
+    @Size(min = 5,max = 24,message = "password must be at least 5 characters and at most 24 characters")
+    private String password;
+    private String rePassword;
+
+
+
+
+}
--- auth-service/src/main/java/com/hrm/exception/ErrorType.java
+++ auth-service/src/main/java/com/hrm/exception/ErrorType.java
@@ -9,17 +9,19 @@ import org.springframework.http.HttpStatus;
 @NoArgsConstructor
 @Getter
 public enum ErrorType {
+
+    EMAIL_DUPLICATE(4001,"E-mail is already exist",HttpStatus.BAD_REQUEST),
+    PASSWORD_UNMATCH(4002,"Passwords are not matched",HttpStatus.BAD_REQUEST),
+    INVALID_TOKEN(5001,"Token not created",HttpStatus.BAD_REQUEST),
     INTERNAL_ERROR(5100,"Sunucu Hatası",HttpStatus.INTERNAL_SERVER_ERROR),
-    BAD_REQUEST(4100,"Parametre Hatası",HttpStatus.BAD_REQUEST) ,
-    LOGIN_ERROR(4110,"Kullancı adı veya sifre hatali!!!",HttpStatus.BAD_REQUEST),
-    USERNAME_DUPLICATE(4111,"Böyle bir kullanıcı adı mevcut",HttpStatus.BAD_REQUEST),
-    USER_NOT_FOUND(4112,"Böyle bir kullanıcı bulunamadi" ,HttpStatus.BAD_REQUEST),
-    ACTIVATE_CODE_ERROR(4113,"Aktivasyon kod hatası" ,HttpStatus.BAD_REQUEST),
-    INVALID_TOKEN(4114,"Geçersiz Token" ,HttpStatus.BAD_REQUEST),
-    TOKEN_NOT_CREATED(4115,"Token Oluşturalamadı" ,HttpStatus.BAD_REQUEST),
-    NOT_ACTIVATED_ACCOUNT(4116,"Aktif olmayan hesap" ,HttpStatus.FORBIDDEN),
-    USER_NOT_CREATED(4117,"Kullanıcı olusturulamadı" ,HttpStatus.BAD_REQUEST),
-    ROLE_NOT_FOUND(4118,"Role bulunamadı" ,HttpStatus.BAD_REQUEST );
+    BAD_REQUEST(4100,"Parametre Hatası",HttpStatus.BAD_REQUEST),
+
+
+
+
+
+    ;
+
 
     private int code;
     private String message;
