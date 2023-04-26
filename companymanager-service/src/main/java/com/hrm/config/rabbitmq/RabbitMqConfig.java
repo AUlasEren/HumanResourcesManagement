5f71eb426ffb4bbf6a2652901fb5d0a3cb64b480
ulaş eren <ulass._eren97@gmail.com>
2023-04-26 18:51:29 +0300

    config added

--- companymanager-service/src/main/java/com/hrm/config/rabbitmq/RabbitMqConfig.java
+++ companymanager-service/src/main/java/com/hrm/config/rabbitmq/RabbitMqConfig.java
@@ -6,7 +6,9 @@ import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitMqConfig {
     @Value("${rabbitmq.exchange-manager}")
     private String exchangeCompanyManager;
